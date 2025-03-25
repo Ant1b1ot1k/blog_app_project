a24a571bb60f3a9c447528ed335919935a015720
Ant1b1ot1k <prabathreddy001@gmail.com>
2025-03-24 21:46:22 -0400

    Second commit
    Implemented HTML template inheritance, created home, about and the parent base html files. made changes to the views and the settings files to better accommodate the use of template htmls

--- blog_app_project/blog/views.py
+++ blog_app_project/blog/views.py
@@ -1,11 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# dummy data (for now)
+posts = [
+    {
+        'author': 'User 1',
+        'title': 'Blog Post 1',
+        'content': 'First post in the blog page',
+        'date_posted': 'March 01, 2025'
+    },
+    {
+        'author': 'User 2',
+        'title': 'Blog Post 2',
+        'content': 'Second post in the blog page',
+        'date_posted': 'March 02, 2025'
+    }
+]
+
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    # data is passed into the template using the dictionary below
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context=context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    # return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})  # returns http response
